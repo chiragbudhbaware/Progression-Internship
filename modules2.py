1b4abaa7077893a20d82709c6bb362747344fab7
chiragbudhbaware <chiragbudhbaware@gmail.com>
2025-12-30 13:05:40 +0530

    Day 2

---
+++ modules2.py
@@ -0,0 +1,7 @@
+from modules import add, multiply
+
+result_add = add(10, 5)
+result_multiply = multiply(6, 7)
+
+print(f"result of add: {result_add}")
+print(f"result of multiply: {result_multiply}")
---
+++ oop_class.py
@@ -0,0 +1,9 @@
+class Robo:
+    name = "Chitti"
+    category = "AI Humanoid"
+    language = "Multilingual"
+    power = "Super Strength"
+
+robo1 = Robo()
+print(f"Robo Name: {robo1.name}")
+print(f"Robo Category: {robo1.category}")
